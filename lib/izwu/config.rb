eeeff4c384941dd7aac30583bb5a2cf3800e234e
Whyme Lyu <callme5long@gmail.com>
2023-12-19 19:25:23 +0800

    Find config file in XDG_CONFIG_HOME

--- lib/izwu/config.rb
+++ lib/izwu/config.rb
@@ -6,13 +6,20 @@ module Izwu
   module Config
     class << self
       def load_expectations
-        conf = YAML.safe_load_file "#{ENV['HOME']}/.config/izwu/izwu.yaml"
+        conf = YAML.safe_load_file(find_conf_file)
 
         conf['expectations'].reduce Hash.new do |result, e|
           e = Izwu::Expectation.load_from_conf(e)
           result[e.name] = e
           result
         end
+      end
+
+      def find_conf_file
+        %W[
+          #{ENV['XDG_CONFIG_HOME']}/izwu/izwu.yaml
+          #{ENV['HOME']}/.config/izwu/izwu.yaml
+        ].first{|f| File.exist? f }
       end
     end
   end
